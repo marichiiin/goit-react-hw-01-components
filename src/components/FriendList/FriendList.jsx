14148f1b93803fe79281a7ee453e493243f22ccc
Mary Jane Magamba <40849185+marichiiin@users.noreply.github.com>
2024-08-06 04:12:31 +0800

    Update FriendList.jsx

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -2,7 +2,7 @@ export const FriendLists = ({avatar, name, isOnline, id}) => {
     return (
         <li class="item">
             <span class="status"></span>
-            <img class="avatar" src="" alt="User avatar" width="48" />
+            <img class="avatar" src={avatar} alt="User avatar" width="48" />
             <p class="name">{name}</p>
         </li>
     )
