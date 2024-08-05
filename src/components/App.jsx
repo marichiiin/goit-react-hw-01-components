ba8e3b2b976fa94fb06b9bbf253f723ed6e7c8b5
Mari <40849185+marichiiin@users.noreply.github.com>
2024-08-05 16:08:03 +0800

    Starter code

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,14 @@
+import { Profile } from "./Profile/Profile";
+import user from '../data/user.json'
+
 export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      Testing la-ang
-    </div>
-  );
+  return <Profile 
+    username = {user.username}
+    tag = {user.tag}
+    location = {user.location}
+    avatar = {user.avatar}
+    stats = {user.stats}
+  />;
 };
+
+
